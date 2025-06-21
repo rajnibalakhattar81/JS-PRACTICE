90a87e3cd44f3422c5a096642f01a258be723149
rajnibalakhattar <rajnisinha0103@gmail.com>
2025-06-21 09:19:57 +0530

    new files

---
+++ javascript-practice/es6keyfeaturespractice.js
@@ -0,0 +1,41 @@
+
+
+//ES6 Features Practice
+/* // const sum = (a, b) => a + b;
+//const a=2;
+//const b=3;
+const sum = (a, b) => {a + b};
+//sum();
+console.log(sum(2,3)); */
+
+/* // Template literals
+const name = 'John';
+console.log(`Hello, ${name}!`); */
+
+/* //Classes
+class Person {
+
+  constructor(name) {
+    this.name = name;
+
+  }
+var name='Rajni';
+console.log(name);
+} */
+
+// DEfault Parameters
+/* function greet(name = 'Guest') {
+  return `Hello, ${name}`;
+}
+console.log(greet());  // Output Hello, Guest!
+*/
+
+//DEstructuring assignments
+/* const [a, b] = [1, 2];
+const {name, age} = {name: 'John', age: 25};
+
+console.log(a); // output 1
+console.log(b); // output 2
+console.log(name); // output John
+console.log(age); // output 25
+ */
---
+++ javascript-practice/primitivedatatypes.js
@@ -0,0 +1,5 @@
+
+//Example Of String
+let name = "John Doe";
+console.log(name);  // Output: John Doe
+console.log(typeof name);  // Output: string
