45b8ca1818028a94f12da4b6ce4de0070a9d6fed
rajnibalakhattar <rajnisinha0103@gmail.com>
2025-06-28 09:20:15 +0530

    added more functions in arrayfunctionpractice file

--- javascript-practice/arrayfunctionpractice.js
+++ javascript-practice/arrayfunctionpractice.js
@@ -11,11 +11,74 @@ function greet(name) {
 let x = 10;
 let y = x;
 y = 20;
-console.log(x);  // Output: 10 (x is unchanged)
+//console.log(x);  // Output: 10 (x is unchanged)
 
 //Example of mutability (non-primitive):
+//example 1 of push()
 let arr1 = [1, 2, 3];
 let arr2 = arr1;
 arr2.push(4);
 arr2.push(5);
-console.log(arr1);  // Output: [1, 2, 3, 4] (arr1 is changed because both arr1 and arr2 refer to the same array)
+//console.log(arr1);  // Output: [1, 2, 3, 4, 5] (arr1 is changed because both arr1 and arr2 refer to the same array)
+
+//example 2 of push()
+let fruits1 = ["apple", "banana"];
+fruits1.push("orange");  
+console.log(fruits1);  // Output: ["apple", "banana", "orange"]
+
+//example of pop()
+let fruits2 = ["apple", "banana", "orange"];
+let removedFruit = fruits2.pop();  
+console.log(removedFruit);  // Output: "orange"
+console.log(fruits2);  // Output: ["apple", "banana"]
+
+//Example of shift()
+
+let fruits3 = ["apple", "banana", "orange"];
+let firstFruit = fruits3.shift();  
+console.log(firstFruit);  // Output: "apple"
+console.log(fruits3);  // Output: ["banana", "orange"]
+
+//example of unshift()
+let fruits4 = ["banana", "orange"];
+fruits4.unshift("apple");  
+console.log(fruits4);  // Output: ["apple", "banana", "orange"]
+
+//example of splice
+let fruits5 = ["apple", "banana", "orange"];
+// Removes 1 element at index 1, and inserts "kiwi" at index 1
+fruits5.splice(1, 1, "kiwi");  
+console.log(fruits5);  // Output: ["apple", "kiwi", "orange"]
+
+//example of slice
+let fruits6 = ["apple", "banana", "orange", "kiwi"];
+let citrus = fruits6.slice(1, 3);  
+console.log(citrus);  // Output: ["banana", "orange"]
+
+//example of concat
+let fruits7 = ["apple", "banana"];
+let moreFruits = ["orange", "kiwi"];
+let allFruits = fruits7.concat(moreFruits);  
+console.log(allFruits);  // Output: ["apple", "banana", "orange", "kiwi"]
+
+//example of indexOf()
+
+let fruits8 = ["apple", "banana", "orange"];
+let index = fruits8.indexOf("banana");  
+console.log("index of banana is "+ index);
+
+//example of includes()
+
+let fruits9 = ["apple", "banana", "orange"];
+let hasBanana = fruits9.includes("banana");  
+let hasKiwi = fruits9.includes("kiwi"); 
+console.log(hasBanana);  // Output: true
+console.log(hasKiwi);  // Output: false
+
+//exampleof ForEach()
+let fruits = ["apple", "banana", "orange"];
+fruits.forEach(fruit => console.log(fruit));
+// Output: 
+// apple
+// banana
+// orange
